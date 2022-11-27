b31b70c55afd1c3c195fc3c33647639828ff4357
GeralmyUrena <geralmyuri11@gmail.com>
2022-11-26 23:51:20 -0500

    commit tree da2497ca58940373e0a897b32964e70cd9305bbc

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+const cats = ["Milo","Otis","Garfield"]
+
+function destructivelyAppendCat (cat){
+    cats.push(cat)
+}
+function destructivelyPrependCat(cat){
+    cats.unshift(cat)
+}
+
+function destructivelyRemoveLastCat(cat){
+    cats.pop()
+
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+
+function appendCat(cat){
+   return [...cats, cat]
+   //addedcat.push(cat)
+
+}
+
+function prependCat (cat){
+    return [cat, ...cats]
+}
+function removeLastCat(){
+    const copyCat = [...cats]
+       copyCat.pop()
+       return copyCat
+}
+
+function removeFirstCat(){
+    const anotherC = [...cats]
+    anotherC.shift()
+    return anotherC
+}
